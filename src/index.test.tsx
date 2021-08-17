e4921b29f741c9f5b67ef4fab21192dd91c65e1d
dipin-anand <dipin.anand@solace.com>
2021-08-17 13:13:15 -0400

    Package publishing changes

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-// import { ExampleComponent } from '.'
+import SolaceButton from "./components/SolaceButton";
 
-// describe('ExampleComponent', () => {
-//   xit('is truthy', () => {
-//     expect(ExampleComponent).toBeTruthy()
-//   })
-// })
+describe("ExampleComponent", () => {
+	it("is truthy", () => {
+		expect(SolaceButton).toBeTruthy();
+	});
+});
