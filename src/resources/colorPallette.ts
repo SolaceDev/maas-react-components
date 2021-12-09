9e7844bfd0faa7742e27d1eb82b5481e65770134
Steve Thomas <steven_thomas_12@hotmail.com>
2021-12-08 22:28:05 -0500

    fixing table row boarders

--- src/resources/colorPallette.ts
+++ src/resources/colorPallette.ts
@@ -36,7 +36,8 @@ export const BASE_COLORS = {
 		grey20: "#2D2D2D",
 		grey21: "#474747",
 		grey22: "#828282",
-		grey23: "#EEE"
+		grey23: "#EEE",
+		grey24: "#e5e5e5"
 	},
 	reds: {
 		red1: "#D0021B"
