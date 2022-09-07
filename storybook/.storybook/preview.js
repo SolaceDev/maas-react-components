bffb96b496f8f8233500277976954b47809b29ac
BO LIAO <94377200+bol444@users.noreply.github.com>
2022-09-07 12:03:27 -0400

    DATAGO-39049: Chromatic font loading (#374)
    * increase chromatic version to the lastest
    
    * added font.ready API and preload google fonts in storybook

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,21 +1,24 @@
 import { createTheme, ThemeProvider, SolaceTheme, SupportedThemes } from "@SolaceDev/maas-react-components";
 import { withDesign } from "storybook-addon-designs";
 import { ReactComponent as SolaceSvgs } from "@solacedev/maas-icons/dist/svg/sprite.symbol.svg";
+import isChromatic from "chromatic/isChromatic";
 
 export const decorators = [
 	(Story) => {
 		let themeName = SupportedThemes.solace;
-		if (document.body.classList.contains('sap')) {
+		if (document.body.classList.contains("sap")) {
 			themeName = SupportedThemes.sap;
-		} else if (document.body.classList.contains('new-solace')) {
+		} else if (document.body.classList.contains("new-solace")) {
 			themeName = SupportedThemes.newSolace;
 		}
-		return <ThemeProvider theme={createTheme(SolaceTheme(themeName))}>
-			<div style={{ display: "none" }}>
-				<SolaceSvgs />
-			</div>
-			{Story()}
-		</ThemeProvider>
+		return (
+			<ThemeProvider theme={createTheme(SolaceTheme(themeName))}>
+				<div style={{ display: "none" }}>
+					<SolaceSvgs />
+				</div>
+				{Story()}
+			</ThemeProvider>
+		);
 	},
 	withDesign
 ];
@@ -25,11 +28,19 @@ export const parameters = {
 		expanded: true
 	},
 	themes: {
-		default: 'solace',
+		default: "solace",
 		list: [
-		  { name: 'Solace', color: '#00CCAD' },
-		  { name: 'SAP', class: 'sap', color: '#0a6ed1' },
-		  { name: 'New Solace', class: 'new-solace', color: '#00C895' }
-		],
-	  }
+			{ name: "Solace", color: "#00CCAD" },
+			{ name: "SAP", class: "sap", color: "#0a6ed1" },
+			{ name: "New Solace", class: "new-solace", color: "#00C895" }
+		]
+	}
 };
+
+// Use the document.fonts API to check if fonts have loaded
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/fonts API to
+const fontLoader = async () => ({
+	fonts: await document.fonts.ready
+});
+
+export const loaders = isChromatic() && document.fonts ? [fontLoader] : [];
