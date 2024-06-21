3d531d20fd2c9c712d54884676a7d9166cbd2614
lgh-solace <101145690+lgh-solace@users.noreply.github.com>
2024-06-21 16:42:06 -0400

    fix: adding specifier to paper root in dialog (#633)

--- src/resources/theme.ts
+++ src/resources/theme.ts
@@ -1167,7 +1167,7 @@ const getThemeOptions = (themeName: SupportedThemes) => {
 						".MuiBackdrop-root": {
 							backgroundColor: themeMapping.palette.secondary.w8040
 						},
-						".MuiPaper-root": {
+						".MuiPaper-root:first-child": {
 							minWidth: "400px",
 							maxHeight: "80%",
 							boxShadow: `0 2px 8px ${getBoxShadow("w30")}`,
